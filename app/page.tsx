ef7f634b3a9468e70b6bd10c1e264ff9afdb536b
Davi Marques <142530449+DaviMarques-16@users.noreply.github.com>
2025-09-01 21:59:13 -0300

    chore: adc shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 // SERVER COMPONENTS = sem interatividade
 
-'use client'  //transforma em client component
-
-import { useState } from 'react'
+import { Button } from "./_components/ui/button"
 
 const Home = () => {
-    const [] = useState();
     return (
-            <h1 className='text-red-500'>Home Page</h1>
+           <Button>TESTE</Button>
     )
 }
 

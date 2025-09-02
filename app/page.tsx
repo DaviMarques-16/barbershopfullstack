a5a682ab84f45a8128984f32417f8574833bc9f4
Davi Marques <davimarques22558800@gmail.com>
2025-09-02 15:52:38 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
 // SERVER COMPONENTS = sem interatividade
+// A HOME PAGE
 
+import Header from "./_components/header" //sem chaves ({}) aqui (import components)
+import Image from "next/image"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
 
 const Home = () => {
-    return (
-           <Button>TESTE</Button>
-    )
+  return (
+    <div>
+      {/* HEADER */}
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Davi!</h2>
+        <p>Terça-feira, 02 de setembro.</p>
+
+        <div className="mt-3 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button className="bg-indigo-500">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Home;
+export default Home

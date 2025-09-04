ea2080436dc31e8e2ff16632e6a9f2ff9f49f7a4
Davi Marques <davimarques22558800@gmail.com>
2025-09-04 14:08:17 -0300

    feat: add BarberShopItem component

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -1,6 +1,9 @@
 import { Barbershop } from "../generated/prisma"
+import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image"
+import { Badge } from "./ui/badge";
+import { StarIcon } from "lucide-react"
 
 interface BarbershopItemProps {
     barbershop: Barbershop
@@ -8,15 +11,31 @@ interface BarbershopItemProps {
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
     return (
-        <Card className="w-[159px]">
-            <CardContent className="p-0">
+        <Card className="w-[167px] rounded-2xl">
+            <CardContent className="p-0 px-1 pt-1">
                 {/* IMAGEM */}
                 <div className="relative h-[159px] w-[159px]">
                     <Image 
                     alt={barbershop.name} 
                     fill 
-                    className="object-cover" 
+                    className="object-cover rounded-2xl" 
                     src={barbershop.imageURL} />
+
+                <Badge className="absolute left-2 top-2 gap-1" variant="secondary">
+                    <StarIcon size={12} className="fill-primary text-primary text-indigo-500"/>
+                    <p className="text-xs font-semibold">5,0</p>
+                </Badge>
+                </div>
+
+               
+
+                {/* TEXTO */}
+                <div className="py-3 px-2">
+                    <h3 className="font-semibold truncate">{barbershop.name}</h3>
+                    <p className="text-sm text-gray-400 truncate">{barbershop.address}</p>
+                    <Button variant="secondary" className="w-full mt-3">
+                        Reservar
+                    </Button>
                 </div>
             </CardContent>
         </Card>

d70db50ee55aec2ac6ab9fc3396252b8e1a71cfc
Davi Marques <davimarques22558800@gmail.com>
2025-09-05 16:16:07 -0300

    feat: adc babearias (initial page)

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "./ui/card";
 import Image from "next/image"
 import { Badge } from "./ui/badge";
 import { StarIcon } from "lucide-react"
+import Link from "next/link";
 
 interface BarbershopItemProps {
     barbershop: Barbershop
@@ -33,8 +34,10 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
                 <div className="py-3 px-2">
                     <h3 className="font-semibold truncate">{barbershop.name}</h3>
                     <p className="text-sm text-gray-400 truncate">{barbershop.address}</p>
-                    <Button variant="secondary" className="w-full mt-3">
+                    <Button variant="secondary" className="w-full mt-3" asChild>
+                        <Link href={`/barbershops/${barbershop.id}`}>
                         Reservar
+                        </Link>
                     </Button>
                 </div>
             </CardContent>

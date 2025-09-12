d5ce05508103970cf3f3cdfe53e29e74ff8d3fee
Davi Marques <davimarques22558800@gmail.com>
2025-09-12 16:28:29 -0300

    feat: reserva: inicio

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -1,13 +1,19 @@
+'use client'
+
 import { BarbershopService } from "../generated/prisma"
 import Image from "next/image"
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Calendar } from "./ui/calendar";
+import { ptBR } from "date-fns/locale"
 
 interface ServiceItemProps {
     service: BarbershopService
 }
 
 const ServiceItem = ({service}: ServiceItemProps) => {
+    const [selectedDate, setSelectedDay] = useState<Date | null>(null)
     return (  
     <Card>
       <CardContent className="flex items-center gap-3 p-3">
@@ -32,10 +38,49 @@ const ServiceItem = ({service}: ServiceItemProps) => {
                         currency: 'BRL'
                     }).format(Number(service.price))}
                     </p>
-
-                    <Button variant="secondary" size="sm">
-                        Reservar
-                    </Button>
+                    
+                    <Sheet>
+                      <SheetTrigger>
+                         <Button variant="secondary" size="sm">
+                            Reservar
+                         </Button>
+                      </SheetTrigger>
+                      <SheetContent>
+                        <SheetHeader>
+                            <SheetTitle>Fazer Reserva</SheetTitle>
+                        </SheetHeader>
+                        
+                        <div className="py-5">
+                            <Calendar 
+                            mode="single" 
+                            locale={ptBR}
+                            styles={{
+                                head_cell: {
+                                width: "100%",
+                                textTransform: "capitalize",
+                                },
+                                cell: {
+                                width: "100%",
+                                },
+                                button: {
+                                width: "100%",
+                                },
+                                nav_button_previous: {
+                                width: "32px",
+                                height: "32px",
+                                },
+                                nav_button_next: {
+                                width: "32px",
+                                height: "32px",
+                                },
+                                caption: {
+                                textTransform: "capitalize",
+                                },
+                                }} 
+                      />
+                        </div>
+                      </SheetContent>
+                    </Sheet>
                 </div>
             </div>
       </CardContent>
